772b19fab8b8ee7110907755e82f052064c69e99
daavid.jobe@gmail.com <daavid.jobe@gmail.com>
2021-09-13 13:54:33 +0200

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,38 @@
-import commonjs from "@rollup/plugin-commonjs"
-import resolve from "@rollup/plugin-node-resolve"
-import peerDepsExternal from "rollup-plugin-peer-deps-external"
-import typescript from "@rollup/plugin-typescript"
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from '@rollup/plugin-typescript'
+import url from 'rollup-plugin-url'
+import styles from 'rollup-plugin-styles'
 
-import packageJson from "./package.json"
+import packageJson from './package.json'
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
+      format: 'cjs',
+      sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
+      format: 'esm',
+      sourcemap: true,
+    },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript()
-  ]
+    typescript(),
+    url({
+      // by default, rollup-plugin-url will not handle font files
+      include: ['**/*.woff', '**/*.woff2'],
+      // setting infinite limit will ensure that the files
+      // are always bundled with the code, not copied to /dist
+      limit: Infinity,
+    }),
+    styles(),
+  ],
 }
